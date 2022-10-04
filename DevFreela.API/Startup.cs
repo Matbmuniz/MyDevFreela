9064dad3edaa236dc276e17e2cd484ee1d27fa3f
Matheus Muniz <matheussk2015@gmail.com>
2022-10-03 23:47:17 -0300

    Criacao e introducao do CQRS e MediatR no projeto

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -1,7 +1,9 @@
 using DevFreela.API.Models;
+using DevFreela.Application.Commands.CreateProject;
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistence;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,8 +34,6 @@ namespace DevFreela.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.Configure<OpeningTimeOption>(Configuration.GetSection("OpeningTime"));
-
             var connectString = Configuration.GetConnectionString("DevFreelaCs");
             services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectString));
 
@@ -45,6 +45,8 @@ namespace DevFreela.API
             
 
             services.AddControllers();
+
+            services.AddMediatR(typeof(CreateProjectCommand));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
